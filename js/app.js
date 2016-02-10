b889988bd2c9585a8a9e48d85dbcd576a0228579
massi16894 <massimiliano_pizzolato@hotmail.it>
2016-02-10 15:48:52 +0100

    Execute exercise

--- js/app.js
+++ js/app.js
@@ -13,7 +13,33 @@ var ParlaModel = {
     */
    getLanguageScore : function (phrase, language){
      // write your code here, but don't add any additional 
-     // parameters to the the function
+     // parameters to the the function     
+       var num = 0;
+      
+            
+           if(language == "Italiano" ){
+               for(var j =0; j<ita.length;j++){
+                    if(phrase == ita[j]){
+                        num++;
+                    }
+               }
+           }
+       if(language == "Inglese" ){
+               for(var j =0; j<eng.length;j++){
+                    if(phrase == eng[j]){
+                        num++;
+                    }
+               }
+           }
+       if(language == "Spagnolo" ){
+               for(var j =0; j<spa.length;j++){
+                    if(phrase == spa[j]){
+                        num++;
+                    }
+               }
+           }
+
+       return num;
    },
   
    /* Returns the name of the language in which the phrase
@@ -26,3 +52,35 @@ var ParlaModel = {
    }
          
 };
+
+
+$(document).ready(function(){
+    
+    
+    
+    $("button").on("click", function(){
+        
+        var parola = $("input").val();
+        
+        var italiano = ParlaModel.getLanguageScore(parola,"Italiano");
+        var inglese = ParlaModel.getLanguageScore(parola,"Inglese");
+        var spagnolo = ParlaModel.getLanguageScore(parola,"Spagnolo");
+        alert(italiano);
+        
+        if((italiano > inglese) && (italiano > spagnolo)){
+            $(".lang-name").text("Italiano");
+        }
+        if((inglese > italiano) && (inglese > spagnolo)){
+            $(".lang-name").text("Inglese");
+        }
+        if((spagnolo > italiano) && (spagnolo > inglese)){
+            $(".lang-name").text("Spagnolo");
+        }
+                                                  
+    });
+    
+    
+    
+        
+  
+});
